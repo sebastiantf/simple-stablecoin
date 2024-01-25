94192d626d3688dea128ec294bdf10bc88a1d1d9
Sebastian T F <sebastian.tharakan97@gmail.com>
2024-01-25 18:05:37 +0530

    feat: add `depositCollateral()`

--- src/SimpleStablecoinSystem.sol
+++ src/SimpleStablecoinSystem.sol
@@ -1,4 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-contract SimpleStablecoinSystem {}
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract SimpleStablecoinSystem {
+    using SafeERC20 for IERC20;
+
+    mapping(address user => mapping(address collateral => uint256 collateralBalance)) internal collateralBalances;
+
+    function depositCollateral(address _collateral, uint256 _amount) external {
+        collateralBalances[msg.sender][_collateral] += _amount;
+        IERC20(_collateral).safeTransferFrom(msg.sender, address(this), _amount);
+    }
+
+    function collateralBalanceOf(address _user, address _collateral) public view returns (uint256) {
+        return collateralBalances[_user][_collateral];
+    }
+}
--- test/SimpleStablecoinSystem.t.sol
+++ test/SimpleStablecoinSystem.t.sol
@@ -3,13 +3,38 @@ pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 import {SimpleStablecoinSystem} from "../src/SimpleStablecoinSystem.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
 contract SSDTest is Test {
     SimpleStablecoinSystem public sss;
+    ERC20Mock public weth = new ERC20Mock();
 
     address alice = makeAddr("alice");
 
     function setUp() public {
         sss = new SimpleStablecoinSystem();
+
+        weth.mint(alice, 1 ether);
+    }
+
+    function test_depositCollateral() public {
+        vm.startPrank(alice);
+
+        // initial balances
+        assertEq(sss.collateralBalanceOf(alice, address(weth)), 0);
+        assertEq(weth.balanceOf(alice), 1 ether);
+        assertEq(weth.balanceOf(address(sss)), 0);
+
+        // deposit collateral
+        uint256 collateralAmount = 0.5 ether;
+        weth.approve(address(sss), collateralAmount);
+        sss.depositCollateral(address(weth), collateralAmount);
+
+        // final balances
+        assertEq(sss.collateralBalanceOf(alice, address(weth)), collateralAmount);
+        assertEq(weth.balanceOf(alice), 0.5 ether);
+        assertEq(weth.balanceOf(address(sss)), 0.5 ether);
+
+        vm.stopPrank();
     }
 }
