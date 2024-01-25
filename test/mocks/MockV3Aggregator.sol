03967669624baf2a0f94ff98ca8d56642bc225fa
Sebastian T F <sebastian.tharakan97@gmail.com>
2024-01-26 00:08:54 +0530

    test: store eth price in MockV3Aggregator

--- test/mocks/MockV3Aggregator.sol
+++ test/mocks/MockV3Aggregator.sol
@@ -2,8 +2,10 @@
 pragma solidity ^0.8.13;
 
 contract MockV3Aggregator {
-    function latestRoundData() external pure returns (uint80, int256, uint256, uint256, uint80) {
+    int256 public price = 2000e8;
+
+    function latestRoundData() external view returns (uint80, int256, uint256, uint256, uint80) {
         // return 2000 USD / ETH
-        return (0, 2000e8, 0, 0, 0);
+        return (0, price, 0, 0, 0);
     }
 }
